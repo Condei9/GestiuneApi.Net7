de470bc2013adaa88a829e5c2d549fb31b876fa7
adam1299 <teotenis9@gmail.com>
2023-05-09 23:47:37 +0300

    o am si mare dar o am si mica uneori

--- GestiuneApi.Net7/Models/RoomModel.cs
+++ GestiuneApi.Net7/Models/RoomModel.cs
@@ -4,10 +4,7 @@ namespace GestiuneSaliNET7.Models
 {
     public class RoomModel : Entity
     {
-        [Required]
-        int id { get; set; }
-        [Required]
-        public string name { get; set; }
+       
         [Required]
         public int Capacity { get; set; }
     }
